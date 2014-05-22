0a90f90f1f8d42d8a4b3ca772762131c89610be1
chris-hall-hu-cheng <chris@clockwork-language.co.uk>
2014-05-22 20:04:37 +0000

    Add git master branch for local development

--- drupal8_codio_install.sh
+++ drupal8_codio_install.sh
@@ -5,15 +5,19 @@ parts install php5 php5-apache2 php5-pdo-mysql php5-gd mysql composer
 # Ensure that Mysql and Apache are running
 parts start mysql apache2
 
-# Run everything for the Data base
+# Run everything for the Database
 mysql -u root < ./create_database.sql
 
-# Fetch Drupal
+# Fetch Drupal, get 8.x and create a master branch
 cd /home/codio/workspace
 git remote add drupal8 http://git.drupal.org/project/drupal.git
 git fetch drupal8 8.x
-git checkout -b 8.x
+git checkout  8.x
 git pull drupal8 8.x
+git checkout -b master
+
+# Pull in the standard git ignores 
+cp -p example.gitignore .gitignore
 
 # Install drush
 cd /home/codio
