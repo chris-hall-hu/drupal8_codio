d11552a85bd8742b9a6606aecd1aa3a8bc8a75b1
chris-hall-hu-cheng <chris@clockwork-language.co.uk>
2015-09-01 07:34:40 +0000

    Start a new approach to cloning Drupal branch or tag

--- drupal_codio_install.sh
+++ drupal_codio_install.sh
@@ -7,31 +7,25 @@ parts install php5 php5-apache2 php5-pdo-mysql php5-gd php5-zlib php5-pcntl mysq
 # Get the latest version of composer
 composer selfupdate
 
-# Put the Codio startup script in place to make sure apache and mysql are
-# started each time the box is accessed.
-cp -pr ./startup.sh ../workspace/
 
 # Ensure that Mysql and Apache are running
 parts start mysql apache2
 
 # Run everything for the Database
-mysql -u root < ./create_database.sql
+#mysql -u root < ./create_database.sql
 
-# Check to see if there is any code in workspace yet
-
-if [ ! -f /home/codio/test/index.php ]; then
-  # Fetch Drupal, get 8.x and create a master branch
+# Check to see if there is any Drupal code in workspace yet
+if [ ! -f /home/codio/workspace/index.php ]; then
+  # Fetch Drupal, based on the branch or tag.
   cd /home/codio/workspace
-  git remote add drupal http://git.drupal.org/project/drupal.git
+
+  # Remove any existing Git repro.
+  rm -Rf .git
 
   if [ "" == "$tag" ]; then
-    git fetch drupal $branch
-    git checkout $branch
-    git pull drupal $branch
-    git checkout -b master
+    git clone --branch $branch http://git.drupal.org/project/drupal.git .
   else
-    git fetch drupal -t
-    git checkout $tag                                                                                     
+    git clone --branch $tag http://git.drupal.org/project/drupal.git .                                                                            
   fi              
 
   # Pull in the standard git ignores 
@@ -45,8 +39,8 @@ cd drush
 composer install
 
 # Make drush globally available
-# Todo: it woud be nicer if drush could be installed with composer global install
-#   but that doesn't appear to work on Codio
+# Todo: it would be nicer if drush could be installed with composer global install
+# but that doesn't appear to work on Codio.
 cd /home/codio/.parts/bin 
 ln -s /home/codio/drush/drush drush
 
@@ -55,4 +49,6 @@ cd /home/codio/workspace
 # Install Drupal
 drush -y si standard --db-url=mysql://drupal:drupal@localhost/drupal --site-name=drupal.codio --account-name=admin --account-pass=admin --account-mail=admin@drupal8.codio
 
-
+# Put the Codio startup script in place to make sure apache and mysql are
+# started each time the box is accessed.
+cp -pr ./startup.sh ../workspace/
